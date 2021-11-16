b2eb34748b11507604335c021716dd36ac3d8b7f
Louise <yishinlin@outlook.com>
2021-11-16 23:20:32 +0800

    + add component and filter
    - restaurant api data component
    - basic select and tab
    - search filter

--- src/App.js
+++ src/App.js
@@ -3,11 +3,17 @@ import axios from 'axios';
 import jsSHA from 'jssha';
 
 import { useState, useEffect } from 'react';
+import Restaurant from './Restaurant';
 
 function App() {
 
   const [data, setData] = useState([]);
-  const [currentCity, setCity] = useState(['臺北市', 'Taipei']);
+  const [currentCity, setCity] = useState('Taipei');
+  const [searchInput, setSearchInput] = useState('');
+  const [topic, setTopic] = useState('ScenicSpot');
+  const [restaurantlength,setRestaurantlength] = useState(0)
+
+
   const citys =
     [
       ['臺北市', 'Taipei'],
@@ -33,9 +39,10 @@ function App() {
       ['臺東縣', 'TaitungCounty'],
     ]
 
+
   useEffect(() => {
     axios
-      .get(`https://ptx.transportdata.tw/MOTC/v2/Tourism/ScenicSpot/${currentCity[1]}`, {
+      .get(`https://ptx.transportdata.tw/MOTC/v2/Tourism/ScenicSpot/${currentCity}`, {
         headers: getAuthorizationHeader()
       })
       .then(function (response) {
@@ -43,32 +50,34 @@ function App() {
         setData(response.data)
       })
       .catch(error => console.log(error));
-
   }, [currentCity]);
 
-  const listItems = data.map((item) =>
-    <div className='item-card' key={item.ID}>
+  const listItems = data.map((item) => {
+    return (item.Name.includes(searchInput) && <div className='item-card' key={item.ID} >
       <h3>{item.Name}</h3>
 
       <ul className='class-tag'>
+        <li>景點</li>
         {item.Class1 && <li>{item.Class1}</li>}
         {item.Class2 && <li>{item.Class2}</li>}
         {item.Class3 && <li>{item.Class3}</li>}
         {item.Level && <li>{item.Level}</li>}
       </ul>
-      
-      {item.Address&&<p>Address:{item.Address}</p>}
-      {item.Phone&&<p>Phone: {item.Phone}</p>}
-      {item.Picture.PictureUrl1&&<img src={item.Picture.PictureUrl1} alt="" />}
+
+      {item.Address && <p>Address:{item.Address}</p>}
+      {item.Phone && <p>Phone: {item.Phone}</p>}
+      {item.Picture.PictureUrl1 && <img src={item.Picture.PictureUrl1} alt="" />}
       {/* <p>{item.DescriptionDetail}</p> */}
       {item.TicketInfo && <p>票價：{item.TicketInfo}</p>}
       {item.Remarks && <p>注意事項：{item.Remarks}</p>}
       <p>資料更新時間：{item.SrcUpdateTime}</p>
-    </div>
+    </div>)
+  }
+
 
   );
 
-  const city = citys.map(item => <div className='citybtn' key={item} onClick={() => handleCity(item)}>{item[0]}</div>)
+  const city = citys.map(item => <option className='citybtn' key={item} value={item[1]}>{item[0]}</option>)
 
   function getAuthorizationHeader() {
     //  填入自己 ID、KEY 開始
@@ -87,17 +96,43 @@ function App() {
   function handleCity(item) {
     console.log(item)
     setCity(item)
+
+
   }
+
+  const searchItems = (searchValue) => {
+    setSearchInput(searchValue);
+  }
+  function handleTopic(item) {
+
+    setTopic(item)
+
+
+  }
+
   return (
     <div className="App">
+      <input
+        placeholder='Search...'
+        onChange={(e) => searchItems(e.target.value)}
+      />
+      <select value={currentCity} onChange={(e) => handleCity(e.target.value)}>
+        {city}
+      </select>
+      <h1>{citys.filter(item => item[1] === currentCity)[0][0]}</h1>
 
-      {city}
-      <h1 >{currentCity[0]}({listItems.length})</h1>
-      <header className="App-header">
+      <div class="tab">
+        <button class="tablinks" onClick={() => handleTopic('ScenicSpot')}>景點({listItems.length})</button>
+        <button class="tablinks" onClick={() => handleTopic('Restaurant')}>餐廳({restaurantlength})</button>
 
-        {listItems}
+      </div>
+      {topic === 'Restaurant' && <Restaurant searchInput={searchInput} currentCity={currentCity} setRestaurantlength={setRestaurantlength}/>}
+      {topic === 'ScenicSpot' && <div>
+        <hr />
+        <header className="App-header">
+          {listItems}
+        </header></div>}
 
-      </header>
     </div>
   );
 }
---
+++ src/Restaurant.js
@@ -0,0 +1,72 @@
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+import jsSHA from 'jssha';
+
+
+const Restaurant =({currentCity,searchInput,setRestaurantlength})=>{
+    
+    const [data, setData] = useState([]);
+
+
+    function getAuthorizationHeader() {
+        //  填入自己 ID、KEY 開始
+        let AppID = '368ef97460a54b2ea2f20dc70fc219e8';
+        let AppKey = 'uCykbicq1p1HTtyvdgUhtRY0NKU';
+        //  填入自己 ID、KEY 結束
+        let GMTString = new Date().toGMTString();
+        let ShaObj = new jsSHA('SHA-1', 'TEXT');
+        ShaObj.setHMACKey(AppKey, 'TEXT');
+        ShaObj.update('x-date: ' + GMTString);
+        let HMAC = ShaObj.getHMAC('B64');
+        let Authorization = 'hmac username=\"' + AppID + '\", algorithm=\"hmac-sha1\", headers=\"x-date\", signature=\"' + HMAC + '\"';
+        return { 'Authorization': Authorization, 'X-Date': GMTString };
+      }
+
+
+    useEffect(() => {
+        axios
+          .get(`https://ptx.transportdata.tw/MOTC/v2/Tourism/Restaurant/${currentCity}`, {
+            headers: getAuthorizationHeader()
+          })
+          .then(function (response) {
+            console.log(response.data)
+            setData(response.data)
+          })
+          .catch(error => console.log(error));
+      }, [currentCity])
+     
+
+      const listItems = data.map((item) => {
+        return (item.Name.includes(searchInput) && <div className='item-card' key={item.ID} >
+          <h3>{item.Name}</h3>
+    
+          <ul className='class-tag'>
+            <li>餐廳</li>
+            {item.Class && <li>{item.Class}</li>}
+          
+          </ul>
+    
+          {item.Address && <p>Address:{item.Address}</p>}
+          {item.Phone && <p>Phone: {item.Phone}</p>}
+          {item.Picture.PictureUrl1 && <img src={item.Picture.PictureUrl1} alt="" />}
+          {/* <p>{item.DescriptionDetail}</p> */}
+          {item.TicketInfo && <p>票價：{item.TicketInfo}</p>}
+          {item.Remarks && <p>注意事項：{item.Remarks}</p>}
+          <p>資料更新時間：{item.SrcUpdateTime}</p>
+        </div>)
+      })
+      setRestaurantlength(listItems.length)
+
+    return(
+       <div>
+       
+        <hr />
+        <header className="App-header">
+          {listItems}
+        </header>
+        </div>
+
+    )
+}
+
+export default Restaurant;
